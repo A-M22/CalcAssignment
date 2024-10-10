f9ec3498ce1b3411f702a66f0b89966e3da3f07c
AM <AM@AM>
2024-10-10 20:45:41 +0300

    Add project files.

---
+++ CalcAssignment/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace CalcAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.Minos = new System.Windows.Forms.Button();
+            this.Times = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.FieldBox = new System.Windows.Forms.RichTextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.BackSpace = new System.Windows.Forms.Button();
+            this.ShowBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(11, 85);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(75, 49);
+            this.Num1.TabIndex = 1;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Num1_Click);
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(92, 85);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(75, 49);
+            this.Num2.TabIndex = 2;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Num2_Click);
+            // 
+            // Num3
+            // 
+            this.Num3.Location = new System.Drawing.Point(173, 85);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(75, 49);
+            this.Num3.TabIndex = 3;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Num3_Click);
+            // 
+            // Num4
+            // 
+            this.Num4.Location = new System.Drawing.Point(12, 140);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(75, 49);
+            this.Num4.TabIndex = 4;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Num4_Click);
+            // 
+            // Num5
+            // 
+            this.Num5.Location = new System.Drawing.Point(93, 140);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(75, 49);
+            this.Num5.TabIndex = 5;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Num5_Click);
+            // 
+            // Num6
+            // 
+            this.Num6.Location = new System.Drawing.Point(173, 140);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(75, 49);
+            this.Num6.TabIndex = 6;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Num6_Click);
+            // 
+            // Num7
+            // 
+            this.Num7.Location = new System.Drawing.Point(11, 195);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(75, 49);
+            this.Num7.TabIndex = 7;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Num7_Click);
+            // 
+            // Num8
+            // 
+            this.Num8.Location = new System.Drawing.Point(92, 195);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(75, 49);
+            this.Num8.TabIndex = 8;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Num8_Click);
+            // 
+            // Num9
+            // 
+            this.Num9.Location = new System.Drawing.Point(173, 195);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(75, 49);
+            this.Num9.TabIndex = 9;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Num9_Click);
+            // 
+            // Num0
+            // 
+            this.Num0.Location = new System.Drawing.Point(12, 250);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(236, 49);
+            this.Num0.TabIndex = 10;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(254, 85);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(75, 49);
+            this.Plus.TabIndex = 11;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // Divide
+            // 
+            this.Divide.Location = new System.Drawing.Point(254, 140);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(75, 49);
+            this.Divide.TabIndex = 12;
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = true;
+            this.Divide.Click += new System.EventHandler(this.Divide_Click);
+            // 
+            // Minos
+            // 
+            this.Minos.Location = new System.Drawing.Point(254, 195);
+            this.Minos.Name = "Minos";
+            this.Minos.Size = new System.Drawing.Size(75, 49);
+            this.Minos.TabIndex = 13;
+            this.Minos.Text = "-";
+            this.Minos.UseVisualStyleBackColor = true;
+            this.Minos.Click += new System.EventHandler(this.Minos_Click);
+            // 
+            // Times
+            // 
+            this.Times.Location = new System.Drawing.Point(254, 250);
+            this.Times.Name = "Times";
+            this.Times.Size = new System.Drawing.Size(75, 49);
+            this.Times.TabIndex = 14;
+            this.Times.Text = "X";
+            this.Times.UseVisualStyleBackColor = true;
+            this.Times.Click += new System.EventHandler(this.Times_Click);
+            // 
+            // Equal
+            // 
+            this.Equal.Location = new System.Drawing.Point(335, 195);
+            this.Equal.Name = "Equal";
+            this.Equal.Size = new System.Drawing.Size(75, 104);
+            this.Equal.TabIndex = 15;
+            this.Equal.Text = "=";
+            this.Equal.UseVisualStyleBackColor = true;
+            this.Equal.Click += new System.EventHandler(this.Equal_Click);
+            // 
+            // FieldBox
+            // 
+            this.FieldBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.FieldBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FieldBox.Location = new System.Drawing.Point(8, 30);
+            this.FieldBox.Name = "FieldBox";
+            this.FieldBox.Size = new System.Drawing.Size(402, 49);
+            this.FieldBox.TabIndex = 16;
+            this.FieldBox.Text = "0";
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.Transparent;
+            this.Clear.Location = new System.Drawing.Point(335, 85);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 49);
+            this.Clear.TabIndex = 17;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // BackSpace
+            // 
+            this.BackSpace.Location = new System.Drawing.Point(335, 140);
+            this.BackSpace.Name = "BackSpace";
+            this.BackSpace.Size = new System.Drawing.Size(75, 49);
+            this.BackSpace.TabIndex = 18;
+            this.BackSpace.Text = "Back";
+            this.BackSpace.UseVisualStyleBackColor = true;
+            this.BackSpace.Click += new System.EventHandler(this.BackSpace_Click);
+            // 
+            // ShowBox
+            // 
+            this.ShowBox.AutoSize = true;
+            this.ShowBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ShowBox.Location = new System.Drawing.Point(5, 9);
+            this.ShowBox.Name = "ShowBox";
+            this.ShowBox.Size = new System.Drawing.Size(108, 15);
+            this.ShowBox.TabIndex = 19;
+            this.ShowBox.Text = "                                 ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(417, 305);
+            this.Controls.Add(this.ShowBox);
+            this.Controls.Add(this.BackSpace);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.FieldBox);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.Times);
+            this.Controls.Add(this.Minos);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Button Minos;
+        private System.Windows.Forms.Button Times;
+        private System.Windows.Forms.Button Equal;
+        private System.Windows.Forms.RichTextBox FieldBox;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button BackSpace;
+        private System.Windows.Forms.Label ShowBox;
+    }
+}
+
---
+++ CalcAssignment/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcAssignment
+{
+    
+    public partial class Form1 : Form
+    {
+        double StoredValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Num1_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "1";
+        }
+
+        private void Num2_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "2";
+        }
+
+        private void Num3_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "3";
+        }
+
+        private void Num4_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "4";
+        }
+
+        private void Num5_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "5";
+        }
+
+        private void Num6_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "6";
+        }
+
+        private void Num7_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "7";
+        }
+
+        private void Num8_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "8";
+        }
+
+        private void Num9_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "9";
+        }
+
+        private void Num0_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text += "0";
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            FieldBox.Text = "0";
+            ShowBox.Text = "0";
+            StoredValue = 0;
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            StoredValue = double.Parse(FieldBox.Text);
+            ShowBox.Text = FieldBox.Text+" + ";
+            FieldBox.Text = "";
+        }
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            double num = double.Parse(FieldBox.Text);
+            char o = ShowBox.Text[ShowBox.Text.Length - 2];
+            if (o == '+')
+                StoredValue += num;
+            else if (o == '-') 
+                StoredValue -= num;
+            else if (o == 'x') 
+                StoredValue *= num;
+            else if (o == '/')
+            {
+                if (FieldBox.Text == "0")
+                    MessageBox.Show("Can't Devide by Zero!!");
+                else
+                    StoredValue /=num;
+            }
+            
+            FieldBox.Text = StoredValue.ToString();
+           // MessageBox.Show(StoredValue.ToString())
+
+        }
+
+        private void Divide_Click(object sender, EventArgs e)
+        {
+
+            StoredValue=double.Parse(FieldBox.Text); 
+            ShowBox.Text = FieldBox.Text + " / ";
+            FieldBox.Text="0";
+        }
+
+        private void Minos_Click(object sender, EventArgs e)
+        {
+            ShowBox.Text = FieldBox.Text+" - ";
+            StoredValue=double.Parse(FieldBox.Text);
+            FieldBox.Text="0";
+        }
+
+        private void Times_Click(object sender, EventArgs e)
+        {
+            StoredValue = double.Parse(FieldBox.Text);
+            ShowBox.Text = FieldBox.Text + " x ";
+            FieldBox.Text = "0";
+        }
+
+        private void BackSpace_Click(object sender, EventArgs e)
+        {
+            int x = FieldBox.Text.Length - 1;
+            if (x <=0)
+                FieldBox.Text= "0";
+            else
+            FieldBox.Text=FieldBox.Text.Remove(x);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
